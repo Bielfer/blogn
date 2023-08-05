90449e2fe109bd0e5eda396bed2390246c62d8d6
Gabriel Fernandes <bielfer18@gmail.com>
2023-08-05 15:37:38 -0300

    feat: setup inter font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,13 @@ import '~/styles/globals.css';
 import { type FC } from 'react';
 import ClientProviders from '~/app/client-providers';
 import { ClerkProvider } from '@clerk/nextjs';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+});
 
 type Props = {
   children: React.ReactNode;
@@ -17,7 +24,7 @@ const RootLayout: FC<Props> = ({ children }) => {
         },
       }}
     >
-      <html lang="en">
+      <html lang="en" className={inter.className}>
         <body>
           <ClientProviders>{children}</ClientProviders>
         </body>
