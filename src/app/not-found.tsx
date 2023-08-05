e309a5d492c32a133012dbe467fb0536d99f9934
Gabriel Fernandes <bielfer18@gmail.com>
2023-08-05 15:37:53 -0300

    fix: linting error

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,19 +6,20 @@ const Page404 = () => (
     <main className="mx-auto flex w-full max-w-7xl flex-grow flex-col justify-center px-4 sm:px-6 lg:px-8">
       <div className="py-16">
         <div className="text-center">
-          <p className="text-primary-600 text-sm font-semibold uppercase tracking-wide">
+          <p className="text-sm font-semibold uppercase tracking-wide text-primary-600">
             Error 404
           </p>
           <h1 className="mt-2 text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl">
             Page not found
           </h1>
           <p className="mt-2 text-base text-gray-500">
-            Sorry, but it wasn't possible to find the page you were looking for.
+            Sorry, but it was not possible to find the page you were looking
+            for.
           </p>
           <div className="mt-6">
             <MyLink
               href={paths.home}
-              className="text-primary-600 hover:text-primary-500 text-base font-medium"
+              className="text-base font-medium text-primary-600 hover:text-primary-500"
             >
               Home Page<span aria-hidden="true"> &rarr;</span>
             </MyLink>
