7aff0887df7341bd4540691a2b99ea7a2f2b51de
Gabriel Fernandes <bielfer18@gmail.com>
2023-08-05 15:12:18 -0300

    fix: remove no-img-element rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -27,6 +27,7 @@ const config = {
     '@typescript-eslint/no-unsafe-assignment': 'off',
     '@typescript-eslint/no-unsafe-member-access': 'off',
     '@typescript-eslint/no-unsafe-call': 'off',
+    '@next/next/no-img-element': 'off',
   },
 };
 
