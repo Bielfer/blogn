0ea72f25dd0247793de28f25392b8c24f2e72911
Gabriel Fernandes <bielfer18@gmail.com>
2023-08-09 15:36:38 -0300

    feat: formik date component

---
+++ src/constants/dates.ts
@@ -0,0 +1,24 @@
+export const dayAbbreviations = [
+  'SUN',
+  'MON',
+  'TUE',
+  'WED',
+  'THU',
+  'FRI',
+  'SAT',
+];
+
+export const monthNames = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
