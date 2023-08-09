41127a931c1bf0e5b8c03614da9b477a997f5af3
Gabriel Fernandes <bielfer18@gmail.com>
2023-08-09 15:36:05 -0300

    feat: input layout component

---
+++ src/components/input-layout/input-layout.tsx
@@ -0,0 +1,55 @@
+import { ExclamationCircleIcon } from '@heroicons/react/24/solid';
+import clsx from 'clsx';
+import { type ReactNode } from 'react';
+
+interface Props {
+  label?: string | null;
+  error?: string;
+  hint?: string;
+  className?: string;
+  name?: string;
+  children: ReactNode;
+  shadow?: boolean;
+}
+
+const InputLayout = ({
+  label,
+  hint,
+  error,
+  className,
+  name,
+  children,
+  shadow,
+}: Props) => (
+  <div className={className}>
+    {label && (
+      <div className="mb-1 flex justify-between">
+        <label htmlFor={name}>{label}</label>
+        {hint && (
+          <span className="text-sm text-gray-500" id="email-optional">
+            {hint}
+          </span>
+        )}
+      </div>
+    )}
+    <div className={clsx('relative', shadow && 'shadow-sm')}>
+      {children}
+
+      {!!error && (
+        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+          <ExclamationCircleIcon
+            className="h-5 w-5 text-red-500"
+            aria-hidden="true"
+          />
+        </div>
+      )}
+    </div>
+    {!!error && (
+      <p className="mt-2 text-sm text-red-600" id="error">
+        {error}
+      </p>
+    )}
+  </div>
+);
+
+export default InputLayout;
