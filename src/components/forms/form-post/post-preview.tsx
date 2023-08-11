a995a544dce1750c1d52a91c9eaf7b4f2575a777
Gabriel Fernandes <bielfer18@gmail.com>
2023-08-11 16:20:55 -0300

    feat: post preview component

---
+++ src/components/forms/form-post/post-preview.tsx
@@ -0,0 +1,58 @@
+import { type OutputData } from '@editorjs/editorjs';
+import { useField } from 'formik';
+import type { FC } from 'react';
+import editorJsHtml from 'editorjs-html';
+import Container from '~/components/container';
+import Button from '~/components/button';
+import { ArrowUturnLeftIcon } from '@heroicons/react/20/solid';
+import EmptyState from '~/components/empty-state';
+import { PencilSquareIcon } from '@heroicons/react/24/outline';
+
+const toHtml = editorJsHtml();
+
+type Props = {
+  handleClose: () => void;
+};
+
+const PostPreview: FC<Props> = ({ handleClose }) => {
+  const [{ value: title }] = useField<string>('title');
+  const [{ value: data }] = useField<OutputData>('content');
+  const postHasContent = data.blocks.length > 0;
+
+  let html: string[] | undefined;
+
+  if (postHasContent) html = toHtml.parse(data);
+
+  return (
+    <>
+      <Container className="flex justify-end">
+        <Button onClick={handleClose} iconLeft={ArrowUturnLeftIcon}>
+          Go Back
+        </Button>
+      </Container>
+      <Container smallerContainer>
+        <h1 className="py-8">{title}</h1>
+        {postHasContent ? (
+          <div className="prose max-w-full" key={data.time}>
+            {html?.map((item, index) => {
+              if (typeof item === 'string') {
+                return (
+                  <div dangerouslySetInnerHTML={{ __html: item }} key={index} />
+                );
+              }
+              return item;
+            })}
+          </div>
+        ) : (
+          <EmptyState
+            icon={PencilSquareIcon}
+            title="No content was created yet"
+            subtitle="You can start your post by going back and pressing TAB"
+          />
+        )}
+      </Container>
+    </>
+  );
+};
+
+export default PostPreview;
