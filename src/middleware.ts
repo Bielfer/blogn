9b0a624d9fe8336a1cafbadd5e9e484828bf89bd
Gabriel Fernandes <bielfer18@gmail.com>
2023-08-12 17:30:56 -0300

    fix: handle trpc auth in code

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from '@clerk/nextjs';
 import { paths } from './constants/paths';
 
 export default authMiddleware({
-  publicRoutes: [paths.home, paths.newPost],
+  publicRoutes: ['/(api|trpc)(.*)', paths.home, paths.newPost],
 });
 
 export const config = {
