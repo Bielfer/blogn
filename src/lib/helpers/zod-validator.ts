7783c03cbba88b16245f6f75c6ef26443f174d7d
Gabriel Fernandes <bielfer18@gmail.com>
2023-09-10 19:00:52 -0300

    feat: zod validator

---
+++ src/lib/helpers/zod-validator.ts
@@ -0,0 +1,24 @@
+import { type z } from 'zod';
+
+export const zodValidator =
+  <T extends z.ZodTypeAny>(schema: T) =>
+  (values: Record<string, any>) => {
+    const valuesCopy = { ...values };
+
+    Object.entries(valuesCopy).forEach(([key, value]) => {
+      if (value === '') valuesCopy[key] = undefined;
+    });
+
+    const parse = schema.safeParse(valuesCopy);
+
+    if (parse.success) return {};
+
+    const { fieldErrors } = parse.error.flatten();
+    const errors: Record<string, any> = {};
+
+    Object.keys(fieldErrors).forEach((errorKey) => {
+      errors[errorKey] = fieldErrors[errorKey]?.[0];
+    });
+
+    return errors;
+  };
