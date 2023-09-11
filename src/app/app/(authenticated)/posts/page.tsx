0d8e3902151cdf037f5ce8960fe4f02d8ef4a03f
Gabriel Fernandes <bielfer18@gmail.com>
2023-09-11 14:43:50 -0300

    feat: posts page

--- src/app/app/(authenticated)/posts/page.tsx
+++ src/app/app/(authenticated)/posts/page.tsx
@@ -2,85 +2,14 @@ import { type Metadata } from 'next';
 import { type FC } from 'react';
 import MyLink from '~/components/my-link';
 import DashboardNavbar from '~/components/dashboard-navbar';
-import BasicTemplate from '~/components/templates/basic';
 import { dashboardRoutes, routes } from '~/lib/constants/routes';
 import { HiMiniPlus } from 'react-icons/hi2';
+import PostListWrapper from './post-list-wrapper';
 
 export const metadata: Metadata = {
   title: 'Your Posts',
   description: 'Create and edit your blog posts',
 };
-
-const posts = [
-  {
-    id: 1,
-    title: 'Boost your conversion rate',
-    href: '#',
-    description:
-      'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.',
-    date: 'Mar 16, 2020',
-    datetime: '2020-03-16',
-    category: { title: 'Marketing', href: '#' },
-    author: {
-      name: 'Michael Foster',
-      role: 'Co-Founder / CTO',
-      href: '#',
-      imageUrl:
-        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-  },
-  {
-    id: 1,
-    title: 'Boost your conversion rate',
-    href: '#',
-    description:
-      'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.',
-    date: 'Mar 16, 2020',
-    datetime: '2020-03-16',
-    category: { title: 'Marketing', href: '#' },
-    author: {
-      name: 'Michael Foster',
-      role: 'Co-Founder / CTO',
-      href: '#',
-      imageUrl:
-        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-  },
-  {
-    id: 1,
-    title: 'Boost your conversion rate',
-    href: '#',
-    description:
-      'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.',
-    date: 'Mar 16, 2020',
-    datetime: '2020-03-16',
-    category: { title: 'Marketing', href: '#' },
-    author: {
-      name: 'Michael Foster',
-      role: 'Co-Founder / CTO',
-      href: '#',
-      imageUrl:
-        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-  },
-  {
-    id: 1,
-    title: 'Boost your conversion rate',
-    href: '#',
-    description:
-      'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.',
-    date: 'Mar 16, 2020',
-    datetime: '2020-03-16',
-    category: { title: 'Marketing', href: '#' },
-    author: {
-      name: 'Michael Foster',
-      role: 'Co-Founder / CTO',
-      href: '#',
-      imageUrl:
-        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-  },
-];
 
 const Posts: FC = () => {
   return (
@@ -95,7 +24,7 @@ const Posts: FC = () => {
           New Post
         </MyLink>
       </header>
-      <BasicTemplate posts={posts} columns={3} />
+      <PostListWrapper />
     </DashboardNavbar>
   );
 };
---
+++ src/app/app/(authenticated)/posts/post-list-wrapper.tsx
@@ -0,0 +1,36 @@
+'use client';
+import { format } from 'date-fns';
+import type { FC } from 'react';
+import PostsList from '~/components/posts-list';
+import { publicImagesHref } from '~/lib/constants/public';
+import { routes } from '~/lib/constants/routes';
+import { trpc } from '~/lib/trpc';
+import { useUser } from '~/store';
+
+const PostListWrapper: FC = () => {
+  const { user } = useUser();
+  const { data: posts } = trpc.post.getMany.useQuery(
+    { uid: user?.uid },
+    { enabled: !!user?.uid }
+  );
+
+  const formattedPosts =
+    posts?.map((post) => ({
+      id: post.id,
+      title: post.title,
+      href: { pathname: routes.newPost, query: { postId: post.id } },
+      description: post.SEODescription || 'No description provided',
+      date: format(post.updatedAt, 'MMM d, yyyy'),
+      datetime: format(post.updatedAt, 'yyyy-MM-dd'),
+      category: { title: 'Test', href: '/' },
+      author: {
+        name: user?.displayName ?? 'No name provided',
+        role: '',
+        imageUrl: user?.photoURL ?? publicImagesHref.userIcon,
+      },
+    })) ?? [];
+
+  return <PostsList posts={formattedPosts} columns={3} />;
+};
+
+export default PostListWrapper;
