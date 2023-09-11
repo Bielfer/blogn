780a0f32b13133b74c6cc556594f5b68b18088c0
Gabriel Fernandes <bielfer18@gmail.com>
2023-09-11 14:40:16 -0300

    feat: posts get many and create

--- src/server/routers/post.ts
+++ src/server/routers/post.ts
@@ -2,25 +2,80 @@ import { router, privateProcedure } from '~/server/trpc';
 import { z } from 'zod';
 import { tryCatch } from '~/lib/helpers/try-catch';
 import { TRPCError } from '@trpc/server';
+import { db } from '~/services/firebase/admin';
+import { collections } from '~/lib/constants/firebase';
+import {
+  conditionalWheres,
+  formatDocument,
+  snapshotToArray,
+} from '~/lib/helpers/firebase';
+import { createSchema, updateSchema } from '~/lib/helpers/zod';
+
+const postSchema = z.object({
+  id: z.string(),
+  content: z.object({
+    version: z.string().optional(),
+    blocks: z.object({}).array(),
+    time: z.number().optional(),
+  }),
+  title: z.string(),
+  urlTitle: z.string(),
+  SEOTitle: z.string(),
+  SEODescription: z.string(),
+  publishedAt: z.date(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+});
+
+const createPostSchema = createSchema(postSchema);
+const updatePostSchema = updateSchema(postSchema);
 
 export const postRouter = router({
-  create: privateProcedure
+  getMany: privateProcedure
     .input(
       z.object({
-        content: z.object({
-          version: z.string().optional(),
-          blocks: z.object({}).array(),
-          time: z.number().optional(),
-        }),
-        title: z.string(),
-        urlTitle: z.string(),
-        SEOTitle: z.string(),
-        SEODescription: z.string(),
-        publishedAt: z.date(),
+        uid: z.string().optional(),
+        organizationId: z.string().optional(),
       })
     )
-    .mutation(({ input, ctx }) => {
-      return 'temp';
+    .query(async ({ input }) => {
+      const { organizationId, uid } = input;
+
+      const [postsSnapshot, error] = await tryCatch(
+        conditionalWheres(db.collection(collections.posts), [
+          ['organizationId', '==', organizationId],
+          ['uid', '==', uid],
+        ]).get()
+      );
+
+      if (error || !postsSnapshot)
+        throw new TRPCError({ code: 'BAD_REQUEST', message: error });
+
+      const posts = snapshotToArray<z.infer<typeof postSchema>>(postsSnapshot);
+
+      return posts;
+    }),
+  create: privateProcedure
+    .input(createPostSchema)
+    .mutation(async ({ input, ctx }) => {
+      const { uid } = ctx.decodedIdToken;
+
+      const [postRef, error] = await tryCatch(
+        db.collection(collections.posts).add({
+          ...input,
+          uid,
+        })
+      );
+
+      if (!postRef || error)
+        throw new TRPCError({ code: 'BAD_REQUEST', message: error });
+
+      const [postSnapshot, errorGettingPost] = await tryCatch(postRef.get());
+
+      if (!postSnapshot || errorGettingPost)
+        throw new TRPCError({ code: 'BAD_REQUEST', message: errorGettingPost });
+
+      return formatDocument<z.infer<typeof postSchema>>(postSnapshot);
     }),
 });
 
