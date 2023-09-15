9fb8a16858d1a53ccab39a75a721a6f77d3c056c
Gabriel Fernandes <bielfer18@gmail.com>
2023-09-15 17:00:21 -0300

    fix: start trpc context with decoded id token

--- src/server/context.ts
+++ src/server/context.ts
@@ -1,8 +1,17 @@
 /* eslint @typescript-eslint/require-await:off */
 import type * as trpc from '@trpc/server';
+import { type DecodedIdToken } from 'firebase-admin/auth';
+
+type CreateContextOptions = {
+  decodedIdToken: DecodedIdToken | null;
+};
+
+export const createContextInner = async (opts: CreateContextOptions) => ({
+  decodedIdToken: opts.decodedIdToken,
+});
 
 export const createContext = async () => {
-  return {};
+  return createContextInner({ decodedIdToken: null });
 };
 
 export type Context = trpc.inferAsyncReturnType<typeof createContext>;
