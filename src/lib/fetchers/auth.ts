23fa7d5b343e4d389cc579e565b1dabfb464238f
Gabriel Fernandes <bielfer18@gmail.com>
2023-09-16 19:15:56 -0300

    feat: handle redirects inside get user and is authenticated

--- src/lib/fetchers/auth.ts
+++ src/lib/fetchers/auth.ts
@@ -2,20 +2,22 @@ import { cookies } from 'next/headers';
 import { cookiesKeys } from '../constants/cookies';
 import { tryCatch } from '../helpers/try-catch';
 import { auth } from '~/services/firebase/admin';
+import { redirect } from 'next/navigation';
+import { routes } from '../constants/routes';
 
 export const isAuthenticated = async () => {
   const cookieStore = cookies();
   const sessionToken = cookieStore.get(cookiesKeys.session)?.value;
 
   if (!sessionToken) {
-    return null;
+    redirect(routes.appSignIn);
   }
 
   const [decodedSessionToken, error] = await tryCatch(
     auth.verifySessionCookie(sessionToken)
   );
 
-  if (error || !decodedSessionToken) return null;
+  if (error || !decodedSessionToken) redirect(routes.appSignIn);
 
   return decodedSessionToken;
 };
@@ -23,16 +25,23 @@ export const isAuthenticated = async () => {
 export const getUser = async () => {
   const decodedIdToken = await isAuthenticated();
 
-  if (!decodedIdToken) return null;
+  if (!decodedIdToken) return redirect(routes.appSignIn);
 
   const [user, error] = await tryCatch(auth.getUser(decodedIdToken.uid));
 
-  if (!user || error) return null;
+  if (!user || error) return redirect(routes.appSignIn);
+
+  const customClaims = user.customClaims ?? {};
+  const { blogs, selectedBlog } = customClaims ?? {};
 
   return {
     uid: user.uid,
     photoURL: user.photoURL,
-    customClaims: user.customClaims,
+    customClaims: {
+      ...customClaims,
+      blogs: (blogs ?? []) as string[],
+      selectedBlog: (selectedBlog ?? null) as string | null,
+    },
     displayName: user.displayName,
   };
 };
