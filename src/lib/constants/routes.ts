9570970d726db4dfe809cda7dde53cae5afc0f26
Gabriel Fernandes <bielfer18@gmail.com>
2023-09-16 20:53:46 -0300

    feat: create and update blog routes

--- src/lib/constants/routes.ts
+++ src/lib/constants/routes.ts
@@ -4,8 +4,12 @@ export const routes = {
   appPostEdit: (postId: string) => `/posts/${postId}`,
   appSignIn: '/sign-in',
   appSignUp: '/sign-up',
+  appBlogsNew: '/blogs/new',
+  appBlogsNewFirst: '/blogs/new/first',
+  appBlogsSettings: '/blogs/settings',
 } as const;
 
 export const dashboardRoutes = [
   { name: 'Posts', href: routes.appPosts },
+  { name: 'Settings', href: routes.appBlogsSettings },
 ] as const;
