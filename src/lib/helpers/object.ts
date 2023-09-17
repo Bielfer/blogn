8043bcd5a8739163ed20454007346da44efaa8c4
Gabriel Fernandes <bielfer18@gmail.com>
2023-09-17 20:08:08 -0300

    fix: key picker type

--- src/lib/helpers/object.ts
+++ src/lib/helpers/object.ts
@@ -15,7 +15,7 @@ export const keyPicker = <Obj extends object, Key extends keyof Obj>(
   obj: Obj,
   keys: Key[]
 ) => {
-  const objCopy = {} as Record<Key, Obj[Key]>;
+  const objCopy = {} as Pick<Obj, Key>;
 
   for (const key of keys) {
     objCopy[key] = obj[key];
