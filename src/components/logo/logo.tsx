e07e31a93e17f7ee7957251211147e4841a4646e
Gabriel Fernandes <bielfer18@gmail.com>
2023-09-24 18:32:35 -0300

    feat: logo accept extra props

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -2,19 +2,27 @@ import type { FC } from 'react';
 import logoWhiteBlack from 'public/logo-black.png';
 import Image from 'next/image';
 import cn from '~/lib/helpers/cn';
+import Link from 'next/link';
+import { routes } from '~/lib/constants/routes';
 
 type Props = {
   className?: string;
+  href?: string;
+  height?: number;
+  width?: number;
 };
 
-const Logo: FC<Props> = ({ className }) => {
+const Logo: FC<Props> = ({ className, href, height, width }) => {
   return (
-    <Image
-      className={cn(className)}
-      src={logoWhiteBlack}
-      height={71}
-      alt="Blogn logo"
-    />
+    <Link href={href ?? routes.appHome}>
+      <Image
+        className={cn(className)}
+        src={logoWhiteBlack}
+        height={height ?? 71}
+        width={width}
+        alt="Blogn logo"
+      />
+    </Link>
   );
 };
 
