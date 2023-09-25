9f7dca0f27d455dc98c94f1daf0a65c097b258fc
Gabriel Fernandes <bielfer18@gmail.com>
2023-09-25 20:38:56 -0300

    feat: get post by url, get previous and next posts

--- src/lib/fetchers/post.ts
+++ src/lib/fetchers/post.ts
@@ -5,6 +5,7 @@ import { tryCatch } from '../helpers/try-catch';
 import { caller } from '~/server/routers/_app';
 import { type GetPosts } from '~/server/routers/post';
 import { notFound } from 'next/navigation';
+import { postStatus } from '../constants/posts';
 
 export const getPostById = (id: string) =>
   db.collection(collections.posts).doc(id).get();
@@ -13,6 +14,42 @@ export const getPosts = async (query: GetPosts) => {
   const [data, errorPosts] = await tryCatch(caller.post.getMany(query));
 
   if (errorPosts || !data) notFound();
+
+  return data;
+};
+
+export const getPostByUrl = async ({
+  postUrl,
+  blogId,
+}: {
+  postUrl: string;
+  blogId: string;
+}) => {
+  const [data, errorPosts] = await tryCatch(
+    caller.post.getMany({ urlTitle: postUrl, blogId })
+  );
+
+  if (errorPosts || !data || data.posts.length === 0) notFound();
+
+  return data.posts[0]!;
+};
+
+export const getPreviousAndNext = async ({
+  blogId,
+  postId,
+}: {
+  blogId: string;
+  postId: string;
+}) => {
+  const [data, error] = await tryCatch(
+    caller.post.getPreviousAndNext({
+      blogId,
+      status: postStatus.published,
+      postId,
+    })
+  );
+
+  if (error || !data) notFound();
 
   return data;
 };
