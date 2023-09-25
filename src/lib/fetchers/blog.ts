7aa91928e5562f5c9866bafdca48e6185dcfce49
Gabriel Fernandes <bielfer18@gmail.com>
2023-09-24 22:13:45 -0300

    feat: get blog by domain fetcher

--- src/lib/fetchers/blog.ts
+++ src/lib/fetchers/blog.ts
@@ -2,9 +2,11 @@ import { type User } from '~/store/user';
 import { tryCatch } from '../helpers/try-catch';
 import { db } from '~/services/firebase/admin';
 import { collections } from '../constants/firebase';
-import { redirect } from 'next/navigation';
+import { notFound, redirect } from 'next/navigation';
 import { routes } from '../constants/routes';
 import { headers } from 'next/headers';
+import { getDomains } from '../helpers/metadata';
+import { caller } from '~/server/routers/_app';
 
 export const userHasBlogs = async (user: User) => {
   const pathname = headers().get('pathname');
@@ -22,4 +24,18 @@ export const userHasBlogs = async (user: User) => {
     pathname !== routes.appBlogsNewFirst
   )
     redirect(routes.appBlogsNewFirst);
+};
+
+export const getBlogByDomain = async (params: { domain: string }) => {
+  const { domain, subdomain } = getDomains(params.domain);
+
+  const [blogs, error] = await tryCatch(
+    caller.blog.getMany({
+      ...(!!subdomain ? { subdomain } : { domain }),
+    })
+  );
+
+  if (error || !blogs || blogs.length === 0) notFound();
+
+  return blogs[0]!;
 };
