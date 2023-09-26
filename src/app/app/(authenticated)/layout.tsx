a87783266a0faeecc19e063d35f204f297e5083b
Gabriel Fernandes <bielfer18@gmail.com>
2023-09-26 16:20:36 -0300

    fix: improve auth fetchers returns

--- src/app/app/(authenticated)/layout.tsx
+++ src/app/app/(authenticated)/layout.tsx
@@ -1,14 +1,19 @@
-import type { FC, ReactNode } from 'react';
+import type { ReactNode } from 'react';
+import type { FC } from 'react';
 import { getUser } from '~/lib/fetchers/auth';
 import SetUser from './set-user';
 import { userHasBlogs } from '~/lib/fetchers/blog';
+import { redirect } from 'next/navigation';
+import { routes } from '~/lib/constants/routes';
 
-type Props = {
+export type Props = {
   children: ReactNode;
 };
 
-const AuthenticatedLayout: FC<Props> = async ({ children }) => {
+export const AuthenticatedLayout: FC<Props> = async ({ children }) => {
   const user = await getUser();
+
+  if (!user) redirect(routes.appSignIn);
 
   await userHasBlogs(user);
 
--- src/lib/fetchers/auth.ts
+++ src/lib/fetchers/auth.ts
@@ -2,22 +2,22 @@ import { cookies } from 'next/headers';
 import { cookiesKeys } from '../constants/cookies';
 import { tryCatch } from '../helpers/try-catch';
 import { auth } from '~/services/firebase/admin';
-import { redirect } from 'next/navigation';
-import { routes } from '../constants/routes';
 
 export const isAuthenticated = async () => {
   const cookieStore = cookies();
   const sessionToken = cookieStore.get(cookiesKeys.session)?.value;
 
   if (!sessionToken) {
-    redirect(routes.appSignIn);
+    return;
   }
 
   const [decodedSessionToken, error] = await tryCatch(
     auth.verifySessionCookie(sessionToken)
   );
 
-  if (error || !decodedSessionToken) redirect(routes.appSignIn);
+  if (error || !decodedSessionToken) {
+    return;
+  }
 
   return decodedSessionToken;
 };
@@ -25,11 +25,11 @@ export const isAuthenticated = async () => {
 export const getUser = async () => {
   const decodedIdToken = await isAuthenticated();
 
-  if (!decodedIdToken) return redirect(routes.appSignIn);
+  if (!decodedIdToken) return;
 
   const [user, error] = await tryCatch(auth.getUser(decodedIdToken.uid));
 
-  if (!user || error) return redirect(routes.appSignIn);
+  if (!user || error) return;
 
   return {
     uid: user.uid,
