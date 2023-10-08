cee61ab51f3ade207af2173a0d3592a7197f40f4
Gabriel Fernandes <bielfer18@gmail.com>
2023-10-07 23:18:22 -0300

    fix: bug where there were 2 a tags

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -35,10 +35,10 @@ const Navbar: FC<Props> = ({
     <header className={cn('bg-white', className)}>
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
+          <div className="-m-1.5 p-1.5">
             <span className="sr-only">Blogn Logo</span>
             <Logo />
-          </a>
+          </div>
         </div>
         {onMiddle}
         <div className="flex lg:hidden">
@@ -71,10 +71,10 @@ const Navbar: FC<Props> = ({
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
+            <div className="-m-1.5 p-1.5">
               <span className="sr-only">Blogn</span>
               <Logo />
-            </a>
+            </div>
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-gray-700"
